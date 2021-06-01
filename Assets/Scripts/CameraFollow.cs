10e09b3833bb11cd4142721c784b6873946428ea
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-06-01 09:37:00 +0200

    implemented camera turning

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,22 +7,26 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform m_FollowTransform;
     [SerializeField] private float m_DistanceOffGround = 10f;
     [SerializeField] private float m_DistanceFromFollow = 10f;
-    [SerializeField] private float m_StartingYAngle = 45f;
+    [SerializeField] private float m_CameraStartingAngle = 45f;
+    [SerializeField] private float m_CameraTurnSpeed = 1f;
     
 
     private Camera m_MainCamera;
+    private Vector3 m_Offset;
+    private float m_CameraAngle;
     
 
     private void Awake()
     {
+        m_CameraAngle = m_CameraStartingAngle * Mathf.Deg2Rad;
         m_MainCamera = GetComponent<Camera>();
     }
 
     void FixedUpdate()
     {
-        float angle = m_StartingYAngle * Mathf.Deg2Rad;
-        Vector3 offset = new Vector3(m_DistanceFromFollow * Mathf.Cos(angle), m_DistanceOffGround, m_DistanceFromFollow * Mathf.Sin(angle));
-        this.transform.position = m_FollowTransform.position + offset;
+        m_CameraAngle += Input.GetAxisRaw("CameraHorizontal") * m_CameraTurnSpeed * Time.deltaTime;
+        m_Offset = new Vector3(m_DistanceFromFollow * Mathf.Cos(m_CameraAngle), m_DistanceOffGround, m_DistanceFromFollow * Mathf.Sin(m_CameraAngle));
+        this.transform.position = m_FollowTransform.position + m_Offset;
         this.transform.LookAt(m_FollowTransform);
     }
 }
