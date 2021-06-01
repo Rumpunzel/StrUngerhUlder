6773dfd218f69da3a51bc58a6c1f74888bd1057e
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-06-01 08:50:59 +0200

    improved camera placement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform m_FollowTransform;
-    [SerializeField] private Vector3 m_Offset = new Vector3(10f, 10f, 10f);
+    [SerializeField] private float m_DistanceOffGround = 10f;
+    [SerializeField] private float m_DistanceFromFollow = 10f;
+    [SerializeField] private float m_StartingYAngle = 45f;
+    
 
     private Camera m_MainCamera;
     
@@ -17,7 +20,9 @@ public class CameraFollow : MonoBehaviour
 
     void FixedUpdate()
     {
-        this.transform.position = m_FollowTransform.position + m_Offset;
+        float angle = m_StartingYAngle * Mathf.Deg2Rad;
+        Vector3 offset = new Vector3(m_DistanceFromFollow * Mathf.Cos(angle), m_DistanceOffGround, m_DistanceFromFollow * Mathf.Sin(angle));
+        this.transform.position = m_FollowTransform.position + offset;
         this.transform.LookAt(m_FollowTransform);
     }
 }
