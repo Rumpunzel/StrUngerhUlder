fe593694275879d1e69f59c3861bdf64b954f885
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-06-01 08:18:51 +0200

    implemented camera relative movement

--- Assets/Scripts/GameObjects/GameActors/PlayerInput.cs
+++ Assets/Scripts/GameObjects/GameActors/PlayerInput.cs
@@ -6,6 +6,10 @@ public class PlayerInput : MonoBehaviour
 {
     public Vector3 m_Movement = Vector3.zero;
 
+    private Camera m_MainCamera;
+    private Vector3 m_CameraForward;
+    private Vector3 m_CameraRight;
+
 
     void Update()
     {
@@ -14,6 +18,14 @@ public class PlayerInput : MonoBehaviour
 
     protected void GetInputs()
     {
-        m_Movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        m_MainCamera = Camera.main;
+        m_CameraForward = m_MainCamera.transform.forward;
+        m_CameraRight = m_MainCamera.transform.right;
+        m_CameraForward.y = 0f;
+        m_CameraRight.y = 0f;
+        m_CameraForward.Normalize();
+        m_CameraRight.Normalize();
+
+        m_Movement = m_CameraForward * Input.GetAxisRaw("Vertical") + m_CameraRight * Input.GetAxisRaw("Horizontal");
     }
 }
