52b717713372129cb8c976f52e7d8e232daaa883
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-06-10 14:42:47 +0200

    created damagable SOs

--- Assets/Scripts/Characters/Config/DroppableRewardConfigSO.cs
+++ Assets/Scripts/Characters/Config/DroppableRewardConfigSO.cs
@@ -7,12 +7,10 @@ namespace Strungerhulder.Characters.ScriptableObjects
     public class DroppableRewardConfigSO : ScriptableObject
     {
         [Tooltip("Item scattering distance from the source of dropping.")]
-        [SerializeField]
-        private float m_ScatteringDistance = default;
+        [SerializeField] private float m_ScatteringDistance = default;
 
         [Tooltip("The list of drop goup that can be dropped by this critter when killed")]
-        [SerializeField]
-        private List<DropGroup> m_DropGroups = new List<DropGroup>();
+        [SerializeField] private List<DropGroup> m_DropGroups = new List<DropGroup>();
 
         public float ScatteringDistance => m_ScatteringDistance;
         public List<DropGroup> DropGroups => m_DropGroups;
